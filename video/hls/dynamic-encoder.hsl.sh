8eae9be65c837f15be77596c6005c3eee0fd4e24
encoder <encoder@encoder.ilivehost.com>
2016-05-25 18:51:42 -0300

    Commit

--- video/hls/dynamic-encoder.hsl.sh
+++ video/hls/dynamic-encoder.hsl.sh
@@ -33,7 +33,7 @@ ffmpeg  -y -i "$1" \
         -vbsf h264_mp4toannexb \
         -flags -global_header \
         -pix_fmt yuv420p\
-        -c:v libx264 -x264opts 'keyint=48:min-keyint=48:pic-struct:no-scenecut' -movflags fragkeyframe  \
+        -c:v libx264 -x264opts "keyint=$GOP_INTERVAL:min-keyint=$GOP_INTERVAL:pic-struct:no-scenecut" -movflags fragkeyframe  \
         -vprofile ${PROFILE} -level ${LEVEL}  -preset $H264_PRESET \
         -g ${FRAME_RATE} -keyint_min $GOP_INTERVAL\
         -bufsize ${BRATES0}   \
@@ -53,7 +53,7 @@ cat  >> $OUTSCRIPT  <<  "EOF"
    	-filter:v scale=iw*.20:-1 \
 	 -bufsize ${BRATES1}   \
 	-b:v ${BRATES1} -r $FRAME_RATE \
-	-c:v libx264 -x264opts 'keyint=48:min-keyint=48:pic-struct:no-scenecut' -movflags fragkeyframe  \
+	-c:v libx264 -x264opts "keyint=$GOP_INTERVAL:min-keyint=$GOP_INTERVAL:pic-struct:no-scenecut" -movflags fragkeyframe  \
 	-vprofile ${PROFILE} -level ${LEVEL}  -preset $H264_PRESET \
 	-g ${FRAME_RATE} -keyint_min $GOP_INTERVAL\
 	-maxrate ${BRATES1} \
@@ -71,7 +71,7 @@ cat  >> $OUTSCRIPT  <<  "EOF"
         -filter:v "scale=iw*.40:-2" \
 	-bufsize ${BRATES2}   \
         -b:v ${BRATES2} -r $FRAME_RATE \
-        -c:v libx264 -x264opts 'keyint=48:min-keyint=48:pic-struct:no-scenecut' -movflags fragkeyframe  \
+	-c:v libx264 -x264opts "keyint=$GOP_INTERVAL:min-keyint=$GOP_INTERVAL:pic-struct:no-scenecut" -movflags fragkeyframe  \
         -vprofile ${PROFILE} -level ${LEVEL}  -preset $H264_PRESET \
         -g ${FRAME_RATE} -keyint_min $GOP_INTERVAL\
         -maxrate ${BRATES2} \
@@ -89,7 +89,7 @@ cat  >> $OUTSCRIPT  <<  "EOF"
         -filter:v scale=iw*.75:-2 \
 	-bufsize ${BRATES3}   \
         -b:v ${BRATES3} -r $FRAME_RATE \
-        -c:v libx264 -x264opts 'keyint=48:min-keyint=48:pic-struct:no-scenecut' -movflags fragkeyframe  \
+	-c:v libx264 -x264opts "keyint=$GOP_INTERVAL:min-keyint=$GOP_INTERVAL:pic-struct:no-scenecut" -movflags fragkeyframe  \
         -vprofile ${PROFILE} -level ${LEVEL}  -preset $H264_PRESET \
         -g ${FRAME_RATE} -keyint_min $GOP_INTERVAL\
         -maxrate ${BRATES3} \
@@ -107,7 +107,7 @@ cat  >> $OUTSCRIPT  <<  "EOF"
     -filter:v scale=-1:-1 \
         -bufsize ${BRATES4}   \
         -b:v ${BRATES4} -r $FRAME_RATE \
-        -c:v libx264 -x264opts 'keyint=48:min-keyint=48:pic-struct:no-scenecut' -movflags fragkeyframe  \
+	-c:v libx264 -x264opts "keyint=$GOP_INTERVAL:min-keyint=$GOP_INTERVAL:pic-struct:no-scenecut" -movflags fragkeyframe  \
         -vprofile ${PROFILE} -level ${LEVEL}  -preset $H264_PRESET \
         -g ${FRAME_RATE} -keyint_min $GOP_INTERVAL\
         -maxrate ${BRATES4} \
@@ -125,7 +125,7 @@ cat  >> $OUTSCRIPT  <<  "EOF"
         -filter:v scale=-1:-1 \
         -bufsize ${BRATES5}   \
         -b:v ${BRATES5} -r $FRAME_RATE \
-        -c:v libx264 -x264opts 'keyint=48:min-keyint=48:pic-struct:no-scenecut' -movflags fragkeyframe  \
+	-c:v libx264 -x264opts "keyint=$GOP_INTERVAL:min-keyint=$GOP_INTERVAL:pic-struct:no-scenecut" -movflags fragkeyframe  \
         -vprofile ${PROFILE} -level ${LEVEL}  -preset $H264_PRESET \
         -g ${FRAME_RATE} -keyint_min $GOP_INTERVAL\
         -maxrate ${BRATES5} \
@@ -143,7 +143,7 @@ cat  >> $OUTSCRIPT  <<  "EOF"
         -filter:v scale=-1:-1 \
         -bufsize ${ORIG_BITRATE}k   \
         -b:v  ${ORIG_BITRATE}k -r $FRAME_RATE \
-        -c:v libx264 -x264opts 'keyint=48:min-keyint=48:pic-struct:no-scenecut' -movflags fragkeyframe  \
+	-c:v libx264 -x264opts "keyint=$GOP_INTERVAL:min-keyint=$GOP_INTERVAL:pic-struct:no-scenecut" -movflags fragkeyframe  \
         -vprofile ${PROFILE} -level ${LEVEL}  -preset $H264_PRESET \
         -g ${FRAME_RATE} -keyint_min $GOP_INTERVAL\
         -maxrate ${ORIG_BITRATE}k \
@@ -157,6 +157,6 @@ EOF
 fi
 
 
-cat  $OUTSCRIPT
-#source $OUTSCRIPT
-#rm -fv $OUTSCRIPT
+#cat  $OUTSCRIPT
+source $OUTSCRIPT
+rm -fv $OUTSCRIPT
